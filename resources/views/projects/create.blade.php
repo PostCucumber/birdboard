46fa3a3faea7faefe9190b5be5895a3baac35ed0
postCucumber <johnston94@gmail.com>
2019-12-22 14:33:23 -0600

    add view

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,12 +3,26 @@
 <head></head>
 <body>
     <h1>Create a Project</h1>
-    <form action="">
+    <form method="POST" action="/projects">
+        @csrf
         <div>
-            <label for="">Title</label>
+            <label for="title">Title</label>
         </div>
         <div>
             <input type="text" name="title" placeholder="title">
+        </div>
+        <div>
+            <div>
+                <label for="description">Description</label>
+            </div>
+            <div>
+                <textarea name="description" id="" cols="30" rows="10"></textarea>
+            </div>
+        </div>
+        <div>
+            <div>
+                <button type="submit">Create</button>
+            </div>
         </div>
     </form>
 
