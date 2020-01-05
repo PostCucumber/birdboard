6f19f30cc444ad8200e498df78488aadc9c31075
postCucumber <johnston94@gmail.com>
2020-01-05 17:08:12 -0600

    wip

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,14 +1,14 @@
 @extends ('layouts.app')
 @section('content')
     <div class="flex items-center">       
-        <h1 class="mr-auto mb-3">Birdboard</h1>
+        <h1 class="mr-auto mb-3 text-blue-200">Birdboard</h1>
         <a href="/projects/create">Create New Project</a>
     </div>
     <div class="flex">
         @forelse ($projects as $project)
                 <div class="bg-white mr-4 p-8 shadow-md rounded w-1/3">
-                    <h1>{{ $project->title }}</h1>
-                    <div>{{ $project->description }}</div>
+                    <h1 class="text-xl font-semibold py-4">{{ $project->title }}</h1>
+                    <div class="text-gray-700 ">{{ Str::limit($project->description,150) }}</div>
                 </div>
         @empty
             <div>No Projects yet</div>
