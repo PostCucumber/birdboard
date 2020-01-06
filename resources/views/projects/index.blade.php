7770de6d18913fa7247c979868553bffa7476c54
postCucumber <johnston94@gmail.com>
2020-01-05 18:14:18 -0600

    wip

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -7,8 +7,8 @@
     <main class="flex flex-wrap -mx-3">
         @forelse ($projects as $project)
                 <div class="w-1/3 px-3 pb-6">
-                    <div class="bg-white p-8 shadow-md rounded" style="height:16rem">
-                        <h1 class="text-xl font-semibold py-4">{{ $project->title }}</h1>
+                    <div class="bg-white p-5 shadow-md rounded" style="height:16rem">
+                        <h1 class="text-xl font-semibold py-4 -ml-5 border-l-4 border-blue-brand pl-4">{{ $project->title }}</h1>
                         <div class="text-gray-700 pb-4">{{ Str::limit($project->description,150) }}</div>
                     </div>
                 </div>
