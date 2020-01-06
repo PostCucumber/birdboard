e5755f6e3eca911fd1911b65f42582e294b988f4
postCucumber <johnston94@gmail.com>
2020-01-06 10:38:02 -0600

    wip

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,13 +2,15 @@
 @section('content')
     <div class="flex items-center justify-between mb-3">       
         <h2 class="subpixel-antialiased text-gray-500">My Projects</h2>
-        <button class="py-2 px-3 bg-blue-brand text-gray-100 rounded shadow mb-2 tracking-wide font-light"><a href="/projects/create">Add Project</a></button>
+        <button class="button"><a href="/projects/create">Add Project</a></button>
     </div>
-    <main class="flex flex-wrap -mx-3">
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-                <div class="w-1/3 px-3 pb-6">
-                    <div class="bg-white p-5 shadow-md rounded" style="height:16rem">
-                        <h1 class="text-xl font-semibold py-4 -ml-5 border-l-4 border-blue-brand pl-4">{{ $project->title }}</h1>
+                <div class="lg:w-1/3 px-3 pb-6">
+                    <div class="card" style="height:16rem">
+                        <h3 class="text-xl font-semibold py-4 -ml-5 mb-3 border-l-4 border-blue-brand pl-4">
+                            <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                        </h3>
                         <div class="text-gray-700 pb-4">{{ Str::limit($project->description,150) }}</div>
                     </div>
                 </div>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,6 +1,6 @@
 @extends ('layouts.app')
 @section('content')
-    <h1>{{ $project->title }}</h1>
+    <h1 class="text-xl font-semibold">{{ $project->title }}</h1>
     <div>{{ $project->description }}</div>
     <a href="/projects">Go Back</a>
 @endsection
